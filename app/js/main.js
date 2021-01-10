d4371e558067beb8dddb5b122f4a605c28f1fe5a
Woood1 <stryuchkov.2016@mail.ru>
2021-01-10 13:29:55 +0400

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,19 @@
 $(function(){
 
 
+	$(".rate-star").rateYo({
+		rating: 5,
+		readOnly: true,
+		starWidth: "12px"
+	});
+
+$(".product-slider__inner").slick({
+	dots: true,
+	arrows: false,
+	slidesToShow: 2,
+	slidesToScroll: 2
+});
+
+	var mixer = mixitup('.products__inner-box');
+
 });
