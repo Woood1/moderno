60e9eefc741afcc355faaae848d901b4b60917fe
Woood1 <stryuchkov.2016@mail.ru>
2021-01-19 17:02:52 +0400

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -7,12 +7,45 @@ $(function(){
 		starWidth: "12px"
 	});
 
-$(".product-slider__inner").slick({
-	dots: true,
-	arrows: false,
-	slidesToShow: 4,
-	slidesToScroll: 4
-});
+	$(".product-slider__inner").slick({
+		dots: true,
+		arrows: false,
+		slidesToShow: 4,
+		slidesToScroll: 4
+	});
+
+
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 200,
+		to: 500,
+		grid: true
+	});
+
+	$(".icon-th-list").on("click", function(){
+		$(".product__item").addClass("list");
+		$(".icon-th-list").addClass("active");
+		$(".icon-th-large").removeClass("active");
+	});
+	$(".icon-th-large").on("click", function(){
+		$(".product__item").removeClass("list");
+		$(".icon-th-large").addClass("active");
+		$(".icon-th-list").removeClass("active");
+	});
+
+
+
+
+	$(".menu__btn").on("click", function(){
+		$(".menu__list").slideToggle();
+	});
+
+	$(".header__btn-menu").on("click", function(){
+		$(".header__box").toggleClass("active");
+	});
+
 
 	var mixer = mixitup('.products__inner-box');
 
