f475dbc436486ce7086a3c0f080654a873f6417f
Woood1 <stryuchkov.2016@mail.ru>
2021-01-21 23:37:57 +0400

    added settings

--- app/js/main.js
+++ app/js/main.js
@@ -37,16 +37,23 @@ $(function(){
 
 
 
-$('.product-one__tabs .tab').on('click', function(event) {
-var id = $(this).attr('data-id');
-	$('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-	$('.product-one__tabs .tabs').find('.tab').removeClass('active');
-	$(this).addClass('active');
-	$('#'+id).addClass('active-tab').fadeIn();
-	return false;
-});
+	$('.product-one__tabs .tab').on('click', function(event) {
+		var id = $(this).attr('data-id');
+		$('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.product-one__tabs .tabs').find('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('#'+id).addClass('active-tab').fadeIn();
+		return false;
+	});
 
-
+	$('.settings__tabs .tab__two').on('click', function(event) {
+		var id = $(this).attr('data-id');
+		$('.settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.settings__tabs .tabs').find('.tab__two').removeClass('active');
+		$(this).addClass('active');
+		$('#'+id).addClass('active-tab').fadeIn();
+		return false;
+	});
 
 
 
